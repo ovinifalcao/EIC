734f6ee556ebba94c9515c3676c744b62f961094
Monteiro <liandra.monteiro@SPLFCB-NXI12759.local>
2022-06-27 16:24:50 -0300

    NewCards are collapsing and stylized

--- react-project/src/Components/CardLouvor/CardLouvor.jsx
+++ react-project/src/Components/CardLouvor/CardLouvor.jsx
@@ -2,36 +2,77 @@ import './CardLouvor.css';
 import CardFuncao from '../CardFuncao/CardFuncao';
 import CardStreaming from '../CardStreaming/CardStreaming';
 import CardsHeaders from '../CardsHeader/CardsHeader';
+import ItemPlaylist from '../ItemPlaylist/ItemPlaylist';
 
 export default function CardLouvor(props) {
-    return (
-      <div className="dv_Container">
+  return (
+    <div className="dv_Container">
 
-        <CardsHeaders
-          infoDoServico = {props.infoDoCard.servico}
+      <CardsHeaders
+        infoDoServico={props.infoDoCard.servico}
+      />
+
+      <div className='dv_PlaylistContainer'>
+
+        <ItemPlaylist
+          coverMusic="https://upload.wikimedia.org/wikipedia/pt/c/ca/Entre_-_Paulo_C%C3%A9sar_Baruk.jpg"
+          musicTitle="Dependo de Ti"
+          artistName="Baruk"
+          musicTone="Eb"
+          background="#22577E"
+          index={0}
         />
 
-        <div id="dv_SpotifyEmbedded_Louvor">
+        <ItemPlaylist
+          coverMusic="https://upload.wikimedia.org/wikipedia/pt/c/ca/Entre_-_Paulo_C%C3%A9sar_Baruk.jpg"
+          musicTitle="Eu navegarei"
+          artistName="Kemuel"
+          musicTone="Eb"
+          background="#5584AC"
+          index={1}
+        />
+
+        <ItemPlaylist
+          coverMusic="https://upload.wikimedia.org/wikipedia/pt/c/ca/Entre_-_Paulo_C%C3%A9sar_Baruk.jpg"
+          musicTitle="Emaus"
+          artistName="Morada"
+          musicTone="Eb"
+          background="#95D1CC"
+          index={2}
+        />
+
+        <ItemPlaylist
+          coverMusic="https://upload.wikimedia.org/wikipedia/pt/c/ca/Entre_-_Paulo_C%C3%A9sar_Baruk.jpg"
+          musicTitle="Canção de Milena"
+          artistName="ICF Capão"
+          musicTone="Eb"
+          background="#6E85B2"
+          index={3}
+        />
+
+      </div>
+
+      {/* <div id="dv_SpotifyEmbedded_Louvor">
             <iframe src={"https://open.spotify.com/embed/playlist/"+props.infoDoCard.playlist[0].streaming.spotify_Embed+"?utm_source=generator"} width="100%" height="300" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        </div>
+        </div> */}
 
 
-        <div id="dv_StreamingsContainer_Louvor">
+      {/* <div id="dv_StreamingsContainer_Louvor">
             <CardStreaming colorBase="#57BB63" imgLink="spotify" link={props.infoDoCard.playlist[0].link}/>
             <CardStreaming colorBase="#CD201F" imgLink="youtube" link={props.infoDoCard.playlist[2].link}/>
             <CardStreaming colorBase="#FF9400" imgLink="deezer" link={props.infoDoCard.playlist[1].link}/>
-        </div>
+        </div> */}
 
 
-        <div id="dv_FormaçãoContainer_Louvor">
-          {props.infoDoCard.formacao?.map((formacao, i) => (
-            <CardFuncao
-              infoFunca = {formacao}
-              key={i}
-            />
-          ))}
-        </div>
-  
+      <div id="dv_FormaçãoContainer_Louvor">
+        {props.infoDoCard.formacao?.map((formacao, i) => (
+          <CardFuncao
+            infoFunca={formacao}
+            key={i}
+          />
+        ))}
       </div>
-    );
-  }
+
+    </div>
+  );
+}
